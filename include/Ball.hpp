7976819c793be8da19c85e33b8a0ef87e76d7c30
mangokip <144727835+mangokip@users.noreply.github.com>
2025-03-09 17:31:29 -0400

    Add files via upload

---
+++ include/Ball.hpp
@@ -0,0 +1,25 @@
+//
+// Created by bryce-klein on 3/9/25.
+//
+
+#ifndef BALL_HPP
+#define BALL_HPP
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <cstdlib>
+
+class Ball {
+public:
+  explicit Ball(sf::Vector2f position);
+
+  void update(sf::RenderWindow& window);
+  void draw(sf::RenderWindow& window);
+
+private:
+  sf::Vector2f pos{};
+  sf::Vector2f vel{};
+  float gravity;
+  sf::CircleShape circle{};
+};
+
+#endif //BALL_HPP
